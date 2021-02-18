dc4fa900784e8eca2e9b8993c507f0de3cce5614
Khusan Mukhammetrakhimov <khusan.mukhammetrakhimov@gmail.com>
2021-02-18 16:55:29 +0600

    add lesson02 hard

--- script/script.js
+++ script/script.js
@@ -1,21 +1,10 @@
-let money = 20000;
-let income = 'Фриланс';
-let addExpenses = '"Такси","Еда","Комуналка"';
-let deposit = true;
-let mission = 100000;
-let period = 4;
-let budgetDay = money;
+let num = 266219;
+let str = num.toString().split('');
+let sum = 1;
 
-// - Вывести в консоль тип данных значений переменных money, income, deposit;
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-//  - Вывести в консоль длину строки addExpenses
-console.log(addExpenses.length);
-//  - Вывести в консоль “Период равен (period) месяцев” и “Цель заработать (mission) рублей/долларов/гривен/юани”
-console.log(`Период равен ${period} месяцев`  , `Цель заработать ${mission} рублей`);
-// - Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
-console.log(addExpenses.toLocaleLowerCase().split(" "))
-//    - Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30)
-console.log(budgetDay / 30)
-//  - Вывести в консоль budgetDay
+for (let i=0; i < str.length;i++){
+  sum = sum *str[i];
+}
+sum = sum ** 3;
+console.log(sum);
+console.log(sum.toString().slice(0,2));
